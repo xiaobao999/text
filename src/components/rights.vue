84d7921516b9f10fb3d43d8390bbd06bbd688751
xiaobao999 <530405837@qq.com>
2019-02-18 20:07:07 +0800

    修改bug

--- src/components/rights.vue
+++ src/components/rights.vue
@@ -2,13 +2,13 @@
   <el-card class="box">
     <cus-bread level1="权限管理" level2="权限列表"></cus-bread>
     <el-table :data="list" style="width: 100%">
-      <el-table-column label="#" width="50">
-        <template slot-scope="scope">{{scope.row.id-100}}</template>
-      </el-table-column>
+      <el-table-column type="index" label="#" width="50"></el-table-column>
       <el-table-column prop="authName" label="权限名称" width="140"></el-table-column>
       <el-table-column prop="path" label="路径" width="140"></el-table-column>
       <el-table-column label="层级" width="140">
-        <template slot-scope="scope">{{listlevel}}</template>
+        <template>
+          
+        </template>
       </el-table-column>
     </el-table>
   </el-card>
@@ -37,9 +37,6 @@ export default {
       index: 0,
       listlevel: ""
     };
-  },
-  computed: {
-    levellist(list.level) {}
   }
 };
 </script>
